28fd644871a8d7968273c2148e16bb97e8be4c71
Peter <petecruckshank@gmail.com>
2019-03-19 21:06:48 -0400

    added a legend section at top onDblClick() for .todo to change class

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,10 +1,25 @@
 <template>
   <div>
     <h3>Todos</h3>
+    <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span>
+        <span class="incomplete-box"></span> = Incomplete
+      </span>
+      <span>
+        <span class="complete-box"></span> = Complete
+      </span>
+    </div>
     <div class="todos">
-      <div v-for="todo in allTodos" :key="todo.id" class="todo">
+      <div
+        @dblclick="onDblClick(todo)"
+        v-for="todo in allTodos"
+        :key="todo.id"
+        class="todo"
+        v-bind:class="{'is-complete':todo.completed}"
+      >
         {{ todo.title }}
-        <i @click="deleteTodo(todo.id)" class="fa fa-trash-alt"></i>
+        <i @click="deleteTodo(todo.id)" class="fas fa-trash-alt"></i>
       </div>
     </div>
   </div>
@@ -16,7 +31,15 @@ import { mapGetters, mapActions } from "vuex";
 export default {
   name: "Todos",
   methods: {
-    ...mapActions(["fetchTodos", "deleteTodo"])
+    ...mapActions(["fetchTodos", "deleteTodo", "updateTodo"]),
+    onDblClick(todo) {
+      const updTodo = {
+        id: todo.id,
+        title: todo.title,
+        completed: !todo.completed
+      };
+      this.updateTodo(updTodo);
+    }
   },
   computed: mapGetters(["allTodos"]),
   created() {
@@ -67,5 +90,10 @@ i {
 .is-complete {
   background: #35495e;
   color: #fff;
+}
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
 }
 </style>
